d2bfb41218590ad0f9ea5425ac4e972b09b3d82c
AnyMac <anymacstore@MacBook-Air-AnyMac-2.local>
2022-07-15 15:35:59 +0300

    fix

--- src/components/Containers/BackgroundContainer/background-data.ts
+++ src/components/Containers/BackgroundContainer/background-data.ts
@@ -1,19 +1,19 @@
 
 export const backgroundData = [
   {
-    url: "/Backgrounds/envMap.jpg",
+    url: "./Backgrounds/envMap.jpg",
     width: 50,
     height: 50,
     alt: "Ground"
   },
   {
-    url: "/Backgrounds/green.jpeg",
+    url: "./Backgrounds/green.jpeg",
     width: 50,
     height: 50,
     alt: "Green"
   },
   {
-    url: "/Backgrounds/red.jpeg",
+    url: "./Backgrounds/red.jpeg",
     width: 50,
     height: 50,
     alt: "red"
